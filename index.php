ee21a6bb40e158fedc9ef225e51458e8dde45af4
Nguyễn Huy Tập <nghuytap@gmail.com>
2021-10-21 23:47:16 +0700

    update

--- index.php
+++ index.php
@@ -1,10 +1,174 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
     <title>docodemo reha</title>
-  </head>
-  <body>
-
-  </body>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+	<link href="css/animate.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400&family=Be+Vietnam:wght@700&family=Mulish&family=Roboto&display=swap" rel="stylesheet">
+    <link href="css/swiper-bundle.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <a class="logo" href="#"><img src="images/logo.svg"></a>
+        <div class="pull-right">
+        	<a class="btn-register" href="#">ĐĂNG KÝ QUÀ ƯU ĐÃI</a>
+        	<img src="images/vi.svg" width="42">
+        </div>
+    </div>
+    <div class="banner">
+    	<img src="images/banner.png" class="img-responsive">
+    </div>
+    <div class="content">
+    	<div class="box">
+    		<h3>MỨC ĐỘ PHỤC HỒI VÀ VIỆC LUYỆN TẬP HỢP LÝ</h3>
+    		<div class="chart">
+    			<span class="text">
+    				Mức độ<br/>phục hồi
+    			</span>
+    			<img src="images/chart.png" class="img-responsive">
+    		</div>
+    		<p>Luyện tập tích cực, hợp lý trong 180 ngày đầu tiên của tiến trình trị liệu và duy trì kế hoạch luyện tập sau đó rất quan trọng trong việc phục hồi chức năng của bệnh nhân đột quỵ não.</p>
+    	</div>
+    	<div class="hopital container-fluid">
+    		<div class="row">
+    			<div class="col-sm-4">
+    				<div class="hop-content">
+    					<h3>Hợp tác với các bệnh viện công Việt Nam</h3>
+    					<p>Các bác sĩ và chuyên viên từ Tập đoàn Bệnh viện Kitahara đã thực hiện thành công các bài tập phục hồi chức năng và đào tạo nhân lực trình độ cao tại các bệnh viện công tại của Việt Nam, với sự hỗ trợ đặc biệt từ Chính phủ hai nước Việt-Nhật.</p>
+    				</div>
+    			</div>
+    			<div class="col-sm-8">
+    				<div class="swiper myswiper">
+    	  				<div class="swiper-wrapper" id="slide">
+    	  					<div class="swiper-slide"><img src="images/img-1.jpg" class="img-responsive"></div>
+    	  					<div class="swiper-slide"><img src="images/img-2.jpg" class="img-responsive"></div>
+    	  				</div>    					
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    	<div class="feature">
+    		<div class="container">
+    			<h3>3 đặc điểm nổi bật của Docodemo Reha hỗ trợ tối đa tiến trình luyện tập</h3>
+    			<div class="row">
+    				<div class="col-sm-4">
+    					<img src="images/img-step1.png" class="img-responsive">
+    					<h4>
+    						<span>01</span>
+    						Ứng dụng trí thông minh nhân tạo
+    					</h4>
+    					<p>Dựa trên nền tảng là hiểu biết chuyên sâu và kinh nghiệm của Tập đoàn Công nghệ ExaWizards và Bệnh viện Quốc tế Kitahara, cùng dữ liệu thu thập từ hơn 60 cơ sở y tế trên khắp Nhật Bản. </p>
+    				</div>
+    				<div class="col-sm-4">
+    					<img src="images/img-step2.png" class="img-responsive">
+    					<h4>
+    						<span>02</span>
+    						Chuyên viên giàu kinh nghiệm
+    					</h4>
+    					<p>Các chuyên viên được đào tạo chuyên sâu và đánh giá khắt khe theo đúng tiêu chuẩn Nhật Bản, tận tâm tư vấn, hỗ trợ người dùng và ghi nhận tiến trình phục hồi sức khỏe. 	</p>
+    				</div>
+    				<div class="col-sm-4">
+    					<img src="images/img-step3.png" class="img-responsive">
+    					<h4>
+    						<span>03</span>
+    						Bài tập được thiết kế riêng
+    					</h4>
+    					<p>Chuyên viên nghiên cứu bệnh án và thảo luận với người dùng hoặc thân nhân, để xây dựng hệ thống bài tập phù hợp nhất với mục tiêu và thời gian phục hồi.</p>
+    				</div>
+    			</div>
+    			<img src="images/img-3.png" class="img-responsive feature-img">
+			</div>
+    	</div>
+    	<div class="doco">
+    		<div class="container">
+    			<h3>Sử dụng Docodemo Reha dễ như thế nào?</h3>
+    			<p>4 bước thật đơn giản để cùng Trợ lý trị liệu Docodemo Reha phục hồi sức khỏe, tìm lại niềm vui sống mỗi ngày!</p>
+    			<div class="row">
+    				<div class="col-md-3 col-sm-6">
+    					<div class="step"><span>1</span></div>
+    					<img src="images/doco1.svg" class="img-responsive">
+    					<h4>
+    						Đăng ký sử dụng
+    					</h4>
+    					<p>Điền đầy đủ thông tin vào mẫu đăng ký bên dưới và kiểm tra email xác nhận đăng ký thành công </p>
+    				</div>
+    				<div class="col-md-3 col-sm-6">
+    					<div class="step"><span>2</span></div>s
+    					<img src="images/doco2.svg" class="img-responsive">
+    					<h4>
+    						Tải và đăng nhập ứng dụng
+    					</h4>
+    					<p>Khi ứng dụng Docodemo Reha ra mắt vào chúng tôi sẽ gửi đường dẫn tải phần mềm và hướng dẫn đăng nhập đến email bạn đăng ký.</p>
+    				</div>
+    				<div class="col-md-3 col-sm-6">
+    					<div class="step"><span>3</span></div>
+    					<img src="images/doco3.svg" class="img-responsive">
+    					<h4>
+    						Tư vấn trực tuyến
+    					</h4>
+    					<p>Người dùng hoặc thân nhân tham gia buổi tư vấn trực tuyến để thảo luận bệnh án, mục tiêu phục hồi, để chuyên viên xây dựng bài tập thích hợp nhất.</p>
+    				</div>
+    				<div class="col-md-3 col-sm-6">
+    					<div class="step"><span>4</span></div>
+    					<img src="images/doco1.svg" class="img-responsive">
+    					<h4>
+    						Luyện tập và theo dõi
+    					</h4>
+    					<p>Chuyên viên ghi nhận, góp ý, hỗ trợ và động viên người dùng dựa vào dữ liệu được cập nhật, giúp người dùng giữ vững tiến độ phục hồi.</p>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <div class="footer">
+    	<div class="container">
+    		<p>Bạn cần hỗ trợ? Liên hệ ngay với chúng tôi qua</p>
+    		<p><img src="images/fi-sr-call-history.svg"> Tổng đài <a href="tel:">xxx.xxx.xxx</a> (hoạt động 24/7)</p>
+    		<p><img src="images/fi-sr-envelope.svg"> <a href="mailto:">xxxxxx@xxxx.com</a></p>
+    		<p>để nhận được hỗ trợ kịp thời</p>
+    		<div class="bottom">
+    			<div class="row">
+    				<div class="col-sm-4">
+	    				<a href=""><img src="images/logo-footer.png" width="270"></a>
+	    			</div>
+	    			<div class="col-sm-4">
+	    				<p>© 2021 ExaWizards Inc.</p>
+	    			</div>
+	    			<div class="col-sm-4">
+		    			<ul class="social text-right">
+		    				<li><a href="#"><img src="images/icon-fb.svg"></a></li>
+		    				<li><a href="#"><img src="images/icon-linkin.svg"></a></li>
+		    				<li><a href="#"><img src="images/icon-twinter.svg"></a></li>
+		    			</ul>	
+		    		</div>
+		    	</div>
+    		</div>
+    	</div>
+    </div>
+    <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/swiper-bundle.min.js"></script>
+    
+    <script type="text/javascript">
+    	var swiper = new Swiper('.myswiper', {
+			centeredSlides: true,
+	        spaceBetween: 30,
+	        grabCursor: true,
+	        //loop: true,
+	        slidesPerView:2,
+	        /*autoplay: {
+	         	delay: 2500,
+	          	disableOnInteraction: false,
+	        },*/
+		   	pagination: {
+			   	el: '.swiper-pagination',
+		    	clickable: true,
+			},
+		});
+    </script>
+</body>
 </html>
