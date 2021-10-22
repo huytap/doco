938097f319a1324ab531cd5e69fe8abe12d93d92
Tap Nguyen <tap.nguyen@accredoasia.com>
2021-10-22 15:54:09 +0700

    update

--- index.php
+++ index.php
@@ -10,12 +10,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400&family=Be+Vietnam:wght@700&family=Mulish&family=Roboto&display=swap" rel="stylesheet">
     <link href="css/swiper-bundle.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
 </head>
 <body>
     <div class="header">
         <a class="logo" href="#"><img src="images/logo.svg"></a>
         <div class="pull-right">
-        	<a class="btn-register" href="#">ĐĂNG KÝ QUÀ ƯU ĐÃI</a>
+        	<a class="btn-register" href="#dang-ky-nhan-uu-dai">ĐĂNG KÝ QUÀ ƯU ĐÃI</a>
         	<img src="images/vi.svg" width="42">
         </div>
     </div>
@@ -39,6 +40,10 @@
     				<div class="hop-content">
     					<h3>Hợp tác với các bệnh viện công Việt Nam</h3>
     					<p>Các bác sĩ và chuyên viên từ Tập đoàn Bệnh viện Kitahara đã thực hiện thành công các bài tập phục hồi chức năng và đào tạo nhân lực trình độ cao tại các bệnh viện công tại của Việt Nam, với sự hỗ trợ đặc biệt từ Chính phủ hai nước Việt-Nhật.</p>
+						<div id="slide-pagination" class="swiper-pagination">
+							<a class="swiper-prev"><img src="images/icon-arrow-left.png" width="24"></a>
+							<a class="swiper-next"><img src="images/icon-arrow-right.png" width="24"></a>
+						</div>
     				</div>
     			</div>
     			<div class="col-sm-8">
@@ -46,7 +51,7 @@
     	  				<div class="swiper-wrapper" id="slide">
     	  					<div class="swiper-slide"><img src="images/img-1.jpg" class="img-responsive"></div>
     	  					<div class="swiper-slide"><img src="images/img-2.jpg" class="img-responsive"></div>
-    	  				</div>    					
+    	  				</div>
     				</div>
     			</div>
     		</div>
@@ -54,42 +59,42 @@
     	<div class="feature">
     		<div class="container">
     			<h3>3 đặc điểm nổi bật của Docodemo Reha hỗ trợ tối đa tiến trình luyện tập</h3>
-    			<div class="row">
+    			<div class="row">    				
     				<div class="col-sm-4">
     					<img src="images/img-step1.png" class="img-responsive">
     					<h4>
     						<span>01</span>
-    						Ứng dụng trí thông minh nhân tạo
+    						Chuyên viên<br/>giàu kinh nghiệm
     					</h4>
-    					<p>Dựa trên nền tảng là hiểu biết chuyên sâu và kinh nghiệm của Tập đoàn Công nghệ ExaWizards và Bệnh viện Quốc tế Kitahara, cùng dữ liệu thu thập từ hơn 60 cơ sở y tế trên khắp Nhật Bản. </p>
+    					<p>Các chuyên viên được đào tạo chuyên sâu và đánh giá khắt khe theo đúng tiêu chuẩn Nhật Bản, tận tâm tư vấn, hỗ trợ người dùng và ghi nhận tiến trình phục hồi sức khỏe. 	</p>
     				</div>
     				<div class="col-sm-4">
     					<img src="images/img-step2.png" class="img-responsive">
     					<h4>
     						<span>02</span>
-    						Chuyên viên giàu kinh nghiệm
-    					</h4>
-    					<p>Các chuyên viên được đào tạo chuyên sâu và đánh giá khắt khe theo đúng tiêu chuẩn Nhật Bản, tận tâm tư vấn, hỗ trợ người dùng và ghi nhận tiến trình phục hồi sức khỏe. 	</p>
-    				</div>
-    				<div class="col-sm-4">
-    					<img src="images/img-step3.png" class="img-responsive">
-    					<h4>
-    						<span>03</span>
-    						Bài tập được thiết kế riêng
+    						Bài tập được<br/>thiết kế riêng
     					</h4>
     					<p>Chuyên viên nghiên cứu bệnh án và thảo luận với người dùng hoặc thân nhân, để xây dựng hệ thống bài tập phù hợp nhất với mục tiêu và thời gian phục hồi.</p>
     				</div>
+					<div class="col-sm-4">
+    					<img src="images/img-step3.png" class="img-responsive">
+    					<h4>
+    						<span>03</span>
+    						Ứng dụng<br/>trí thông minh nhân tạo
+    					</h4>
+    					<p>Dựa trên nền tảng là hiểu biết chuyên sâu và kinh nghiệm của Tập đoàn Công nghệ ExaWizards và Bệnh viện Quốc tế Kitahara, cùng dữ liệu thu thập từ hơn 60 cơ sở y tế trên khắp Nhật Bản. </p>
+    				</div>
     			</div>
-    			<img src="images/img-3.png" class="img-responsive feature-img">
+    			<!-- <img src="images/img-3.png" class="img-responsive feature-img"> -->
 			</div>
     	</div>
     	<div class="doco">
     		<div class="container">
     			<h3>Sử dụng Docodemo Reha dễ như thế nào?</h3>
-    			<p>4 bước thật đơn giản để cùng Trợ lý trị liệu Docodemo Reha phục hồi sức khỏe, tìm lại niềm vui sống mỗi ngày!</p>
+    			<p class="doco-short">4 bước thật đơn giản để cùng Trợ lý trị liệu Docodemo Reha phục hồi sức khỏe, tìm lại niềm vui sống mỗi ngày!</p>
     			<div class="row">
     				<div class="col-md-3 col-sm-6">
-    					<div class="step"><span>1</span></div>
+    					<div class="step first"><span>1</span></div>
     					<img src="images/doco1.svg" class="img-responsive">
     					<h4>
     						Đăng ký sử dụng
@@ -97,7 +102,7 @@
     					<p>Điền đầy đủ thông tin vào mẫu đăng ký bên dưới và kiểm tra email xác nhận đăng ký thành công </p>
     				</div>
     				<div class="col-md-3 col-sm-6">
-    					<div class="step"><span>2</span></div>s
+    					<div class="step"><span>2</span></div>
     					<img src="images/doco2.svg" class="img-responsive">
     					<h4>
     						Tải và đăng nhập ứng dụng
@@ -113,7 +118,7 @@
     					<p>Người dùng hoặc thân nhân tham gia buổi tư vấn trực tuyến để thảo luận bệnh án, mục tiêu phục hồi, để chuyên viên xây dựng bài tập thích hợp nhất.</p>
     				</div>
     				<div class="col-md-3 col-sm-6">
-    					<div class="step"><span>4</span></div>
+    					<div class="step last"><span>4</span></div>
     					<img src="images/doco1.svg" class="img-responsive">
     					<h4>
     						Luyện tập và theo dõi
@@ -123,6 +128,145 @@
     			</div>
     		</div>
     	</div>
+		<div class="res">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-7">
+						<img src="images/img-4.png" class="img-responsive">
+					</div>
+					<div class="col-sm-5">
+						<div class="res-content">
+							<div class="ct">
+								<h3 class="title">Tương thích với mọi thiết bị</h3>
+								<p>Với sự thấu hiểu tuyệt đối đến nhu cầu luyện tập linh hoạt cho khách hàng. Docodemo Reha được thiết kế để hoạt động tối ưu trên hầu hết tất cả các thiết bị iOS</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="team">
+					<div class="container">
+						<img src="images/logo-exa.png"/ width="410">
+						<h3>Những khối óc và con tim phía sau Docodemo Reha</h3>
+						<p>Nằm trong nhóm 3 đơn vị trị liệu phục hồi sau đột quỵ hiệu quả nhất Tokyo hằng năm, Tập đoàn Y tế Kitahara hướng đến sự phát triển toàn diện, liên tục. Năm 2016, Kitahara thành lập bệnh viện tại Campuchia, khởi đầu cho nhiệm vụ chia sẻ kiến thức trị liệu Nhật Bản với các nước bạn, và tiếp tục triển khai hợp tác với các nước khác, trong đó có Việt Nam. Bên cạnh đó, Kitahara tích cực nghiên cứu và ứng dụng công nghệ thông tin, trí tuệ nhân tạo để nâng cao hiệu quả và chất lượng trị liệu. </p>
+						<p>Mục tiêu của Kitahara không dừng lại ở việc mang dịch vụ trị liệu vượt ra ngoài biên giới Nhật Bản, mà còn hướng đến việc cung cấp kiến thức về vận hành, đào tạo nhân lực, văn hoá thực dưỡng, và ứng dụng công nghệ thông tin vào những khía cạnh đa dạng của đời sống, đặc biệt là y tế.</p>
+						<div class"row">
+							<div class="col-sm-5"><img src="images/team1.png" class="img-responsive"/></div>
+							<div class="col-sm-3"><img src="images/team2.png" class="img-responsive"/></div>
+							<div class="col-sm-4"><img src="images/team3.png" class="img-responsive"/></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="reha" id="dang-ky-nhan-uu-dai">
+			<div class="container">
+				<h3 class="title">Nhận quà tặng khi dùng thử <span>Docodemo Reha</span></h2>
+				<p>
+				Nhận quà tặng tri ân là 2 tháng dùng thử miễn phí khi đăng ký dùng thử Docodemo Reha <span>từ hôm nay đến hết <strong>31/11/2021.</strong></span><br/> 
+				<span><strong>Giải thưởng tổ chức theo hình thức rút thăm, dành cho 15 khách hàng may mắn nhất.</strong></span><br/>
+				Docodemo Reha sẽ gửi email xác nhận trúng giải đến các khách hàng nhận được phần quà này!
+				</p>
+				<form class="form-contact" name="submit-to-google-sheet">
+					<div class="row">
+						<div class="col-sm-6">
+							<input type="text" placeholder="Họ và tên" class="form-control" name="fullname">
+						</div>
+						<div class="col-sm-6">
+							<input type="text" placeholder="Số điện thoại" class="form-control" name="phonenumber">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<input type="email" placeholder="Email" class="form-control" name="email">
+						</div>
+						<div class="col-sm-6">
+							<select name="who_register" class="form-control">
+								<option value="">Bạn đăng ký cho mình hay cho người thân?</option>
+							</select>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<select class="form-control" name="problem">
+								<option value="">Vấn đề bạn thường gặp nhất khi trị liệu từ xa là gì?</option>
+							</select>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<select class="form-control" name="channel">
+								<option value="">Bạn biết đến Docodemo Reha qua kênh nào?</option>
+							</select>
+						</div>
+					</div>
+					<button type="submit" class="btnSubmit">GỬI THÔNG TIN VÀ NHẬN QUÀ!</button>
+				</form>
+			</div>
+		</div>
+		<div class="question">
+			<div class="container">
+				<h3>Các câu hỏi thường gặp</h3>
+				<p>Trợ lý Trị liệu từ xa Docodemo Reha cùng bạn giải đáp một số câu hỏi phổ biến, giúp bạn thêm hiểu và tin tưởng để lựa chọn Docodemo Reha cho mình, người thân và gia đình.</p>
+				<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingOne">
+							<h4 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+								Q: Trị liệu từ xa hiệu quả như thế nào so với phương pháp truyền thống?
+								</a>
+							</h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+							<div class="panel-body">
+								A: Để đạt được hiệu quả như các phương thức luyện tập truyền thống, bạn nên duy trì luyện tập từ xa thường xuyên mỗi ngày, trong một không gian cố định quen thuộc, để tạo thành thói quen trong cuộc sống, giúp thiết lập và duy trì tiến độ phục hồi vững chắc.
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingTwo">
+							<h4 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								Q: Docodemo Reha có chịu trách nhiệm trong trường hợp xảy ra tai nạn khi luyện tập?
+								</a>
+							</h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+							<div class="panel-body">
+								A: Để đạt được hiệu quả như các phương thức luyện tập truyền thống, bạn nên duy trì luyện tập từ xa thường xuyên mỗi ngày, trong một không gian cố định quen thuộc, để tạo thành thói quen trong cuộc sống, giúp thiết lập và duy trì tiến độ phục hồi vững chắc.
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingThree">
+							<h4 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="headingThree">
+								Q: Có nhất thiết phải luyện tập theo đúng kế hoạch đã đặt ra?
+								</a>
+							</h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+							<div class="panel-body">
+								A: Để đạt được hiệu quả như các phương thức luyện tập truyền thống, bạn nên duy trì luyện tập từ xa thường xuyên mỗi ngày, trong một không gian cố định quen thuộc, để tạo thành thói quen trong cuộc sống, giúp thiết lập và duy trì tiến độ phục hồi vững chắc.
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingFour">
+							<h4 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+								Q: Có cần phải luyện tập mỗi ngày không?
+								</a>
+							</h4>
+						</div>
+						<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-expanded="false" aria-labelledby="headingFour">
+							<div class="panel-body">
+								A: Để đạt được hiệu quả như các phương thức luyện tập truyền thống, bạn nên duy trì luyện tập từ xa thường xuyên mỗi ngày, trong một không gian cố định quen thuộc, để tạo thành thói quen trong cuộc sống, giúp thiết lập và duy trì tiến độ phục hồi vững chắc.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
     </div>
     <div class="footer">
     	<div class="container">
@@ -143,7 +287,7 @@
 		    				<li><a href="#"><img src="images/icon-fb.svg"></a></li>
 		    				<li><a href="#"><img src="images/icon-linkin.svg"></a></li>
 		    				<li><a href="#"><img src="images/icon-twinter.svg"></a></li>
-		    			</ul>	
+		    			</ul>
 		    		</div>
 		    	</div>
     		</div>
@@ -152,22 +296,86 @@
     <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/swiper-bundle.min.js"></script>
-    
+	<script type="text/javascript" src="js/jquery.validate.min.js"></script>
     <script type="text/javascript">
     	var swiper = new Swiper('.myswiper', {
-			centeredSlides: true,
 	        spaceBetween: 30,
-	        grabCursor: true,
-	        //loop: true,
-	        slidesPerView:2,
-	        /*autoplay: {
-	         	delay: 2500,
-	          	disableOnInteraction: false,
-	        },*/
-		   	pagination: {
-			   	el: '.swiper-pagination',
-		    	clickable: true,
+	        slidesPerView: "auto",
+			navigation: {
+				nextEl: '.swiper-next',
+				prevEl: '.swiper-prev',
 			},
+		});
+		$(document).ready(function () {
+			var url = location.hash;
+			if(url)
+			$('html,body').animate({scrollTop:$(url).offset().top}, 1700);
+			$(".btn-register").click(function () {
+				$('html,body').animate({scrollTop:$('#dang-ky-nhan-uu-dai').offset().top}, 1700);
+			});
+			//submit form
+			$('form').validate({
+				onfocusout: false,
+				onkeyup: false,
+				onclick: true,
+				ignore: [],
+				rules: {
+					'fullname': {
+						required: true,
+					},
+					'phonenumber': {
+						required: true,
+					},
+					'email': {
+						required: true,
+						email: true
+					},
+					'who_register': {
+						required: true
+					},
+					'problem': {
+						required: true
+					},
+					'channel':{
+						required: true
+					}
+				},
+				messages: {
+					'fullname': {
+						required: 'Vui lòng nhập Họ và Tên',
+					},
+					'phonenumber': {
+						required: 'Vui lòng nhập Số điện thoại',
+					},
+					'email': {
+						required: 'Vui lòng nhập Email',
+						email: 'Email không đúng định dạng'
+					},
+					'who_register': {
+						required: 'Bạn đăng ký cho mình hay cho người thân?'
+					},
+					'problem': {
+						required: 'Vấn đề bạn thường gặp nhất khi trị liệu từ xa là gì?'
+					},
+					'channel':{
+						required: 'Bạn biết đến Docodemo Reha qua kênh nào?'
+					}
+				},
+				errorElement : 'div',
+			});
+			$('.btnSubmit').unbind().click(function(e){
+				e.preventDefault();
+				if($('form').valid()){
+					const scriptURL = 'https://script.google.com/macros/s/AKfycbwcmd4bzliyfPqEu1d15K7B6WD3LGueLveAipZQZM3lKdwnD25NvDdFhXeZTP2lbBrH/exec';
+					const form = document.forms['submit-to-google-sheet']
+					form.addEventListener('submit', e => {
+						e.preventDefault()
+						fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+						.then(response => console.log('Success!', response))
+						.catch(error => console.error('Error!', error.message))
+					})
+				}
+			})
 		});
     </script>
 </body>
